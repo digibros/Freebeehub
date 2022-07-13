ea1f2f84fcd9395c8b0784e0d34e92a59ec52ce2
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2022-07-13 21:25:33 +0530

    home page

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -1,7 +1,9 @@
-import { TextField } from "@mui/material";
+import { Autocomplete, Button, TextField } from "@mui/material";
+import { borderColor } from "@mui/system";
 import React from "react";
 import OwlCarousel from "react-owl-carousel";
 import banner1 from "../../images/banners/banner1.jpg";
+import { top100Films } from "../data/dummy";
 function Main() {
   return (
     <div className="home-main">
@@ -9,9 +11,11 @@ function Main() {
         className="owl-theme"
         loop
         margin={10}
+        autoplay={true}
+        autoplayTimeout={5000}
         responsive={{
           0: {
-            items: 1.1,
+            items: 1.3,
           },
           600: {
             items: 1.3,
@@ -20,20 +24,91 @@ function Main() {
             items: 1,
           },
         }}
-        center
         items={1}
-        nav>
-        <div class="item">
+        nav={false}
+        dots={false}
+      >
+        <div className="item">
+          <img src={banner1} className="carousel-image" />
+        </div>
+        <div className="item">
           <img src={banner1} className="carousel-image" />
         </div>
       </OwlCarousel>
       <div className="sell-form-lg">
-        <TextField
-          // variant="filled"
-          label="Category"
+        <p className="sell-form-title">Sell</p>
+        {/* {/* <Autocomplete
+          className="home-field"
+          disablePortal
           id="category"
-          size="small"
+          options={top100Films}
+          sx={{ width: 300 }}
+          renderInput={(params) => ( 
+            // <TextField
+            //   className="home-field-input"
+            //   {...params}
+            //   label="Product Category"
+            //   fullWidth
+            // /> */}
+        <input
+          className="home-field-input"
+          type="text"
+          name="productDescription"
+          placeholder="Product Category"
         />
+        <input
+          className="home-field-input"
+          type="text"
+          name="productDescription"
+          placeholder="Product Name"
+        />
+        <input
+          className="home-field-input"
+          type="text"
+          name="productDescription"
+          placeholder="Product Description"
+        />
+        {/* )}
+        /> */}
+        {/* <Autocomplete
+          className="home-field"
+          disablePortal
+          id="category"
+          options={top100Films}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+
+          )}
+        /> */}
+        {/* <div className="home-field"> */}
+        {/* <TextField
+            className="home-field-input"
+            label="Product Name"
+            sx={{ width: 286 }}
+          /> */}
+
+        {/* </div>
+        <div className="home-field"> */}
+        {/* <TextField
+            className="home-field-input"
+            label="Product Description"
+            sx={{ width: 286, border: "0" }}
+            size
+          /> */}
+
+        {/* </div> */}
+        <Button
+          variant="outlined"
+          size="large"
+          sx={{
+            width: "80%",
+            marginTop: "15px",
+            color: "#019fbb",
+            borderColor: "#019fbb",
+          }}
+        >
+          SELL
+        </Button>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
+import Categories from "../components/home/Categories";
+import Features from "../components/home/Features";
 import Main from "../components/home/Main";
+import Products from "../components/home/Products";
+import Suggestions from "../components/home/Suggestions";
 import "../css/home.css";
 function Home() {
   return (
     <div>
       <Header />
       <Main />
+      <Categories />
+      <Suggestions />
+      {/* <Features /> */}
+      <Products />
       <Footer />
     </div>
   );
